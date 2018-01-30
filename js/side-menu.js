2b0c6b352c61c414c77b68986530bb2523ec02ea
DESKTOP-FP31GNK\emanu <wallpapershd65@yahoo.com>
2018-01-30 17:46:46 +0200

    Add functionality for side menu

--- js/side-menu.js
+++ js/side-menu.js
@@ -1,36 +1,48 @@
-(function () {
-  $("#nav-overlay").click(function () {
-    $("#side-menu").toggle();
+$(function () {
 
+  $('#nav-overlay').click(function () {  
+    var container = $("#side-menu");       
+    if($('#side-menu').css('display') == 'block') {
+    
+      $(document).mouseup(function(e) {       
+        if (!container.is(e.target) && container.has(e.target).length === 0)  {
+          $( "#side-menu" ).css( "display", "none" );         
+        }
+          $("#nav-overlay").click(function(){
+          $( "#side-menu" ).toggle();
+          
+        });
+    });
+    
+    } else {
+      $( "#side-menu" ).css( "display", "block" );
+    }
   });
 })();
 
 //$(document).mouseup(function(e) {
-//    var container = $("#mySidenav"); 
-//    
+//    var container = $("#side-menu");     
 //    if (!container.is(e.target) && container.has(e.target).length === 0) 
 //    {
-//        container.hide();
+//       container.hide();
 //    }
 //
 //});
 
-//$( "#m" ).click(function() {
-//    $( ".title-anchor" ).css( "font-size", "18px" );
-//    $( ".title-search" ).css( "position", "relative" );
-//    $( ".title-search" ).css( "top", "1" );
-//   
-//});
+$( "#m" ).click(function() {
+    $( ".title-anchor" ).css( "font-size", "18px" );
+    $( ".title-search" ).css( "position", "relative" );
+    $( ".title-search" ).css( "top", "1" );
+   
+});
 
 
-
-
-//  function toggle_visibility(id) {
-//      var menu = document.getElementById(id);
-//      if (menu.style.display == 'block') {
-//          menu.style.display = 'none';
-//      } else {
-//          menu.style.display = 'block';
-//      }
+//function toggle_visibility(id) {
+//  var menu = document.getElementById(id);
+//  if (menu.style.display == 'block') {
+//      menu.style.display = 'none';
+//  } else {
+//      menu.style.display = 'block';
 //  }
+//}
 //
