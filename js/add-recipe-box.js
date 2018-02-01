00b3a1a368d75800ea2af75d2e59d9bc3859872d
Pavel <denev.pavel@gmail.com>
2018-02-01 15:40:07 +0200

    added open meal window, click out of box to close add recipe, added Dobri's js for categories window

--- js/add-recipe-box.js
+++ js/add-recipe-box.js
@@ -1,43 +1,57 @@
-( function () {
-    var $clearAllFields = function () {
-        $( "select" ).val( $( "#default-category" ).val() );
-        $( ".dialog" ).find( "input:text" ).val( "" );
-        $( ".dialog" ).find( "textarea" ).val( "" );
-        $( "#preview" ).attr( "src", "" );
+(function () {
+    var clearAllFields = function () {
+        $("select").val($("#default-category").val());
+        $(".dialog").find("input:text").val("");
+        $(".dialog").find("textarea").val("");
+        $("#preview").attr("src", "");
     }
 
-    $( "#add-recipe" ).on( "click", function () {
-        $( ".dialog" ).css( "display", "block" );
-    } );
+    // open add recipe window with ADD RECIPE button
+    $("#add-recipe").on("click", function () {
+        $(".dialog").css("display", "block");
+        $("#side-menu").css("display", "none");
+        $('.recipesShowModal').css("display", "none");
+        // close add recipe window when clicked anywhere outside the box
+        $(document).mouseup(function (e) {
+            debugger;
+            var container = $('.dialog');
+            // if the target of the click isn't the container nor a descendant of the container
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                container.hide();
+                clearAllFields();
+            }
+        });
+    });
 
-    // var $select = $( "select" );
-    // var $option = $ ( "#default-category" );
-    
-    $( "#close-add-recipe" ).on( "click", function () {
-        $( ".dialog" ).css( "display", "none" );
-        $clearAllFields();
-    } );
+    // close add recipe window with button close 
+    $("#close-add-recipe").on("click", function () {
+        $(".dialog").css("display", "none");
+        clearAllFields();
+    });
 
-    $( "#meal-img-url" ).on( "input", function () {
-        $( "#preview" ).attr( "src", $(this).val());
-    } );
 
-    $( "#save-recipe" ).on( "click", function () {
+
+    // display image when url is entered
+    $("#meal-img-url").on("input", function () {
+        $("#preview").attr("src", $(this).val());
+    });
+
+    $("#save-recipe").on("click", function () {
         var $category = $('.category-select').find(":selected").html();
         var $recipeName = $('.recipe-name').find('input').val();
         var $ingredients = $('.ingredients-container').find('textarea').val().split(',');
         var $description = $('.description-container').find('textarea').val();
         var $imgURL = $('#meal-img-url').val();
 
-        if ($category == 'Select Category' || $recipeName == '' || $ingredients == '' || $description == '' || $imgURL == '' ) {
+        if ($category == 'Select Category' || $recipeName == '' || $ingredients == '' || $description == '' || $imgURL == '') {
             alert('Please, fill in all fields!');
             return false;
         }
-        
-        recipeControl.saveRecipe($category, $recipeName, $ingredients, $description, $imgURL);
-        $clearAllFields();
-        alert('Success!');
-        $( ".dialog" ).css( "display", "none" );
-    } );
 
-} )();
+        recipeControl.saveRecipe($category, $recipeName, $ingredients, $description, $imgURL);
+        clearAllFields();
+        alert('Success!');
+        $(".dialog").css("display", "none");
+    });
+
+})();
