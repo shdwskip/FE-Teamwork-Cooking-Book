aa2bf6b5312a877b519b5b04bbfc801c62177f11
Pavel <denev.pavel@gmail.com>
2018-02-02 19:27:20 +0200

    moveTitle function created, search box moves title now

--- js/side-menu.js
+++ js/side-menu.js
@@ -1,61 +1,57 @@
 $(function () {
 
-  $(document).mouseup(function(e) {  
-  
-    var container = $("#side-menu");   
+  $(document).mouseup(function (e) {
+
+    var container = $("#side-menu");
     var win = $("#meal-window");
-    $('#nav-overlay').click(function () {  
-      
-      $( "#side-menu" ).toggle();
+    $('#nav-overlay').click(function () {
+
+      $("#side-menu").toggle();
     });
-  
-    if (!container.is(e.target) && container.has(e.target).length === 0 && win.has(e.target).length ===0)  {             
-        
-      $( "#side-menu" ).css( "display", "none" );  
-      
-    } 
+
+    if (!container.is(e.target) && container.has(e.target).length === 0 && win.has(e.target).length === 0) {
+
+      $("#side-menu").css("display", "none");
+
+    }
   });
 
-  $( ".menu li" ).click(function() {
-     $( "#title" ).css( "font-size", "50px" );
-     $(".title-search").css("position", "fixed");
-     $(".title-search").css("top", "0");
-     $(".title-search").css("left", "42%");
-     $(".search-box").css("display","none");
-     
-    
+  var moveTitle = function () {
+    $("#title").css("font-size", "3vmax");
+    $(".title-container").css("position", "fixed");
+    $(".title-container").css("top", "0");
+    $(".title-container").css("margin-top", "1%");
+    $(".title-container").css("justify-content", "center");
+    $(".search-box").css("display", "none");
+  }
+  $(".menu li").click(function () {
+    moveTitle();
+
+
   });
-  $( ".addRecipeBtn" ).click(function() {
-    $( "#title" ).css( "font-size", "50px" );
-    $(".title-search").css("position", "fixed");
-    $(".title-search").css("top", "0");
-    $(".title-search").css("left", "42%");
-    $(".search-box").css("display","none");
-   
- });
-   $( "#contact" ).click(function() {
-  $( "#title" ).css( "font-size", "50px" );
-  $(".title-search").css("position", "fixed");
-  $(".title-search").css("top", "0");
-  $(".title-search").css("left", "42%");
-  $(".search-box").css("display","none");
- 
+  $(".addRecipeBtn").click(function () {
+    moveTitle();
+
+  });
+  $("#contact").click(function () {
+    moveTitle();
+  });
+
+
+  $("#contact").on("click", function () {
+    $(".concactInfo").css("display", "block");
+  });
+
+  window.modifyTitle = {moveTitle};
 });
 
- 
- $( "#contact" ).on( "click", function () {
-  $( ".concactInfo" ).css( "display", "block" );
-} );
-  
-  });
 
-  
-  //function toggle_visibility(id) {
-  //  var menu = document.getElementById(id);
-  //  if (menu.style.display == 'block') {
-  //      menu.style.display = 'none';
-  //  } else {
-  //      menu.style.display = 'block';
-  //  }
-  //}
-  //
+//function toggle_visibility(id) {
+//  var menu = document.getElementById(id);
+//  if (menu.style.display == 'block') {
+//      menu.style.display = 'none';
+//  } else {
+//      menu.style.display = 'block';
+//  }
+//}
+//
