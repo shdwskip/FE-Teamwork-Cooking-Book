dfbb523966ae6af74946b0a0f4822db167216266
Pavel <denev.pavel@gmail.com>
2018-02-05 18:18:58 +0200

    moved default recipes to JSON file, renamed default-recipes, removed unnecessary jquery file

---
+++ js/load-default-recipes.js
@@ -0,0 +1,14 @@
+(function () {
+    $.getJSON("js/default-recipes.json", function (data) {
+        debugger;
+        data.defaultRecipes.forEach(function (obj) {
+            obj.id = recipeControl.generateId();
+        })
+        var $defaultRecipes = localStorage.getItem('allRecipes') ?
+            localStorage.getItem('allRecipes') : data.defaultRecipes;
+
+        if (typeof $defaultRecipes !== 'string') {
+            localStorage.setItem('allRecipes', JSON.stringify($defaultRecipes));
+        }
+    })
+})();
